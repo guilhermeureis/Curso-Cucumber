ea8a2af6241df26a602d03108f61bed7ac758886
Guilherme Reis <guilhermeureis@gmail.com>
2021-04-07 21:15:04 -0300

    Dimensionando a tela de teste.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,6 +2,11 @@ Before do
     @login_page = LoginPage.new
     @movie_page = MoviePage.new
     @sidebar = SideBarView.new
+
+    # Maximizar a janela
+    # page.driver.browser.manage.window.maximize
+    # Boa prática é definir o tamanho mínimo da janela
+     page.current_window.resize_to(1440,900)
 end
 
 Before("@login") do
